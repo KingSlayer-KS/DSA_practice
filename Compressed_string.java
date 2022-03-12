4dc20230c6ead70b7dcde1c507d5cdbd4268413e
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-12 23:24:12 +0530

    Sol for 1.6

--- Compressed_string.java
+++ Compressed_string.java
@@ -2,17 +2,17 @@ public class Compressed_string {
     public static int[] map = new int[128];
     public static StringBuilder comp_st = new StringBuilder();
 
-    public static void compression(String k) {
-        for (int i = 0; i < k.length(); i++) {
+    public static void compression(String l) {
+        for (int i = 0; i < l.length(); i++) {
 
-            if (i + 1 >= k.length() || k.charAt(i) != k.charAt(i + 1)) {
-                int val = k.charAt(i);
-                comp_st.append(k.charAt(i));
+            if (i + 1 >= l.length() || l.charAt(i) != l.charAt(i + 1)) {
+                int val = l.charAt(i);
+                comp_st.append(l.charAt(i));
                 comp_st.append(map[val] + 1);
                 map[val] = 0;
 
             } else {
-                int val = k.charAt(i);
+                int val = l.charAt(i);
                 map[val]++;
 
             }
@@ -21,10 +21,10 @@ public class Compressed_string {
     }
 
     public static void main(String[] args) {
-        String k = "aaaaalkea";
+        String l = "aaaaalkea";
 
-        compression(k);
-        if (k.length() > comp_st.length()) {
+        compression(l);
+        if (l.length() > comp_st.length()) {
             System.out.println(comp_st);
         } else {
             System.out.println(comp_st);
