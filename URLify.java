31b9a8204eb2204f32628b92d3ee5902fab1d2b5
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-13 23:11:19 +0400

    Sol for 1.2

--- URLify.java
+++ URLify.java
@@ -4,10 +4,10 @@ public class URLify {
     
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        s.trim();
+        String ss=sc.nextLine();
+        ss.trim();
         sc.close();
-        s=s.replaceAll("\\s","%20");
+        String s=ss.replaceAll("\\s","%20");
         
         System.out.println(s);
         }
