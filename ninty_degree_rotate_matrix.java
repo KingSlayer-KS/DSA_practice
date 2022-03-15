ba9672b514969d648416cbc64ccdcdc46ba4a40d
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-15 21:32:20 +0530

    Sol for 1.7

--- ninty_degree_rotate_matrix.java
+++ ninty_degree_rotate_matrix.java
@@ -1,9 +1,9 @@
 public class ninty_degree_rotate_matrix {
 
-    public static void print_arry(int[][] mat) {
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                System.out.print(mat[i][j] + " ");
+    public static void print_arry(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
 
             }
             System.out.println();
@@ -11,31 +11,31 @@ public class ninty_degree_rotate_matrix {
     }
 
     // transpose of hte array
-    public static void rot(int[][] mat) {
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = i; j < mat.length; j++) {
-                int mat_tem = mat[i][j];
-                mat[i][j] = mat[j][i];
-                mat[j][i] = mat_tem;
+    public static void rot(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length; j++) {
+                int mat_tem = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = mat_tem;
             }
         }
-        // towasds the middle of the aarray and swapping them
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length / 2; j++) {
-                int mat_tem = mat[i][j];
-                mat[i][j] = mat[i][mat.length - j - 1];
-                mat[i][mat.length - j - 1] = mat_tem;
-                System.out.println(mat.length - j - 1);
+        // towards the middle of the aarray and swapping them
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int mat_tem = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = mat_tem;
+                System.out.println(matrix.length - j - 1);
             }
         }
 
     }
 
     public static void main(String[] args) {
-        int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        print_arry(mat);
-        rot(mat);
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        print_arry(matrix);
+        rot(matrix);
         System.out.println();
-        print_arry(mat);
+        print_arry(matrix);
     }
 }
