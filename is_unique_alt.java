bfe288d686c242b6d2acc21b333b8f0cacd7a443
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-17 23:32:19 +0500

    Sol for 1.1

--- is_unique_alt.java
+++ is_unique_alt.java
@@ -3,16 +3,16 @@
 public class is_unique_alt {
     public static boolean is_unique_al(String str) {
 
-        boolean[] is_unique = new boolean[128];
+        boolean[] unique = new boolean[128];
 
         if (str.length() > 128)
             return false;
 
         for (int i = 0; i < str.length(); i++) {
-            if (is_unique[str.charAt(i)]) {
+            if (unique[str.charAt(i)]) {
                 return false;
             }
-            is_unique[str.charAt(i)]= true;
+            unique[str.charAt(i)]= true;
         }
         return true;
     }
