c4beef3db48e506eddb6df07144b5f3800717b63
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-19 23:57:14 +0530

    sol for 2.1

---
+++ Remove_dups.java
@@ -0,0 +1,45 @@
+
+
+class Solution {
+    
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void removedups(ListNode head) {
+        if (head.next == null) {
+            ;
+        }
+        ListNode nzxt = head;
+
+        while (nzxt != null) {
+            // nzxt=nzxt.next;
+            ListNode traverser = nzxt;
+            while (traverser.next != null) {
+                if (traverser.val == nzxt.val) {
+                    nzxt = nzxt.next.next;
+                    
+                }
+                else {
+                    nzxt=nzxt.next;
+                }
+                traverser=traverser.next;
+            }
+
+        }
+
+    }
+}
