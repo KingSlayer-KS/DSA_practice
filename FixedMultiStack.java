9fcd88cde8ab9c01141d265013ab8bfd31c168dc
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-22 20:00:34 +0530

    Sol for 3.1

--- FixedMultiStack.java
+++ FixedMultiStack.java
@@ -68,7 +68,7 @@ public class FixedMultiStack {
 		return values;
 	}
 	
-	public int[] getStackValues(int stackNum) {
+	public int[] getStackValue(int stackNum) {
 		int[] items = new int[sizes[stackNum]];
 		for (int i = 0; i < items.length; i++) {
 			items[i] = values[stackNum * stackCapacity + i];
