e32fad08529c805b2f0c697beff8c94765d29de4
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-23 20:16:12 +0530

    Sol for 3.2

--- Stack_with_min_function.java
+++ Stack_with_min_function.java
@@ -2,49 +2,50 @@ import java.util.Stack;
 
 public class Stack_with_min_function {
     private static Stack<Integer> st;
-    private static Stack<Integer> minSt;
+    private static Stack<Integer> mini;
 
-    private static void push(int x) {
-        // Push x to stack st
-        st.push(x);
-        // If minSt is empty or if top of minSt is greater than x push x to minSt.
-        if (minSt.isEmpty() || minSt.peek() > x) {
-            minSt.push(x);
-        } else {
-            // Else push top of minSt to minSt again.
-            minSt.push(minSt.peek());
+    public static int min() {
+        return mini.peek();
+
+    }
+
+    public static void push(int val) {
+		st.push(val);
+		if (mini.isEmpty() || mini.peek() > val) {
+			mini.push(val);
         }
-    }
 
-    private static void pop() {
-        // Pop element from st and minSt
-        st.pop();
-        minSt.pop();
-    }
+		
 
-    private static int top() {
-        // Return top of st
+	}
+
+    public static int top() {
         return st.peek();
+
     }
 
-    private static int getMin() {
-        // Return top of minSt
-        return minSt.peek();
+    public static void pop() {
+        if (st.peek() == mini.peek()) {
+            st.pop();
+            mini.pop();
+        } else {
+            st.pop();
+
+        }
+
     }
 
     public static void main(String[] args) {
-        // Initialise st and minSt
         st = new Stack<>();
-        minSt = new Stack<>();
-        // Example
-        push(-2);
-        push(0);
-        System.out.println(getMin());
-        push(-3);
-        push(-3);
-        push(-3);
+        mini = new Stack<>();
+        push(-1);
+        push(2);
+        push(3);
+        push(4);
         pop();
+        System.out.println(min());
         System.out.println(top());
-        System.out.println(getMin());
+
     }
+
 }
