050f4baf2676597b791394f4147aad8faebb4b49
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-04-04 00:20:43 +0530

    Sol for 3.6

--- String_rotation.java
+++ String_rotation.java
@@ -2,7 +2,7 @@ public class String_rotation {
     public static boolean st_rot(String s1, String s2) {
         /*max no of rotation for given strings is always a sub string of twice the
         orignal string*/
-        if (s1.length() == s1.length() && (s1 + s1).contains(s2)) {
+        if (s1.length() == s2.length() && (s1 + s1).contains(s2)) {
             return true;
         }
         return false;
