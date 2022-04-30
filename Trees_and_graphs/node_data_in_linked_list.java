990677fcba55009c0bf15f0feb1a43f2524aaef1
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-04-30 12:36:22 -1000

    4.2

---
+++ Trees_and_graphs/node_data_in_linked_list.java
@@ -0,0 +1,75 @@
+/*
+Time Complexity=O(n)
+*/
+package Trees;
+
+import java.util.*;
+
+public class Level_order_traversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;// for traversal
+
+        public Node buildtree(int[] nodes) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node new_node = new Node(nodes[index]);
+            new_node.left = buildtree(nodes);
+            new_node.right = buildtree(nodes);
+
+            return new_node;
+        }
+
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node curr = q.remove();
+            if (curr == null) {
+                System.out.println();
+                // queue empty
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(curr.data + " ");
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildtree(nodes);
+        levelOrder(root);
+    }
+
+}
